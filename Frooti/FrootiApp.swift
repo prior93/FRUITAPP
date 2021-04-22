e618f17f733832b29f1d98616a85b8de32422112
prior93 <Parashar.adhikary@gmail.com>
2021-04-22 12:29:23 +0300

    Initial Commit

---
+++ Frooti/FrootiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrootiApp.swift
+//  Frooti
+//
+//  Created by parashar.r.adhikary on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FrootiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
